f43a9d25d31862fd5626e04e13b5dd09d561ffe3
DocPaintfull <Frederic.kerksieck@rwth-aachen.de>
2020-05-02 16:36:31 +0200

    Vue-devtool support fixed, closing #13

--- app/main.js
+++ app/main.js
@@ -1,7 +1,8 @@
 import Vue from 'nativescript-vue'
+import VueDevtools from 'nativescript-vue-devtools'
+
 import App from './components/App'
 
-import VueDevtools from 'nativescript-vue-devtools'
 
 if(TNS_ENV !== 'production') {
   Vue.use(VueDevtools, { host: '192.168.192.64' })
