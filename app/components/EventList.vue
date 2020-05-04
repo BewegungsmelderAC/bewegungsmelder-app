9398a896ee18ece4f0bbe2b7f033195bcc848782
DocPaintfull <Frederic.kerksieck@rwth-aachen.de>
2020-05-04 06:12:40 +0200

    added fully scrollable list

--- app/components/EventList.vue
+++ app/components/EventList.vue
@@ -2,13 +2,29 @@
   <StackLayout>
     <Button text="Refresh!" @tap="refresh"></Button>
     <Label class="message" :text="eventCount" />
-    <RadListView ref="getEvents" for="event in getEvents" @itemTap="onEventTap">
-      <v-template>
-        <StackLayout>
-          <Label v-if="event" :text="event.name" class="nameLabel"></Label>
-        </StackLayout>
+
+    <RadListView
+              v-if="getEvents" 
+              layout="linear"
+              for="event in getEvents"
+              pullToRefresh="true"
+              @itemTap="onEventTap"
+              @pullToRefreshInitiated="onPullToRefreshInitiated"
+              @scrolled="onScrolled"><!-- maybe define item height for ios -->
+
+      <v-template class="event">
+        <GridLayout columns="*" v-bind:rows="cardHeight">
+          <!-- We don't have to use cardviews -->
+          <card-view if="event.name" margin="10" elevation="40" radius="1">
+            <Stack-layout>
+              <Label :text="event.name" class="nameLabel"></Label>
+            </Stack-layout>
+          </card-view>
+        </GridLayout>
       </v-template>
+
     </RadListView>
+
   </StackLayout>
 </template>
 
@@ -18,16 +34,42 @@
   export default {
     data() {
       return {
-
+        page: 1,
+        scrollOffset: 0,
+        scrollTrigger: 25, // number of cards left unread until new data are loaded
+        cardHeight: 100,
+        perPage: 50,
       };
     },
     methods: {
       refresh() {
-        this.$store.dispatch('fetchEvents', {url: "https://bm-ac.ml/api/event"});
+        this.page = 1;
+        this.$store.dispatch('clearEvents');
+        this.$store.dispatch('fetchEvents', {url: `https://bm-ac.ml/api/event?page=1&per_page=${this.perPage}`});
       },
-      onEventTap() {
+      onScrolled ({ scrollOffset }) {
+        this.scrollOffset = scrollOffset;
+        // Calculate list height and substract unread cards
+        let triggerHeight = (this.page * this.perPage * this.cardHeight) - (this.scrollTrigger * this.cardHeight);
+        if(this.scrollOffset > triggerHeight) {
+          console.log("triggered.");
+          this.page++;
+          this.$store.dispatch('fetchEvents', {url: `https://bm-ac.ml/api/event?page=${this.page}&per_page=${this.perPage}`});
+        }
+      },
+      onEventTap({ item }) {
+        console.log(item);
+      },
+      onPullToRefreshInitiated ({ object }) {
+        this.refresh();
 
-      }
+        // in order to avoid race conditions (only on iOS),
+        // in which the UI may not be completely updated here
+        // we use this.$nextTick call:
+        this.$nextTick(() => {
+          object.notifyPullToRefreshFinished();
+        });
+      },
     },
     computed: {
       ...mapGetters({
