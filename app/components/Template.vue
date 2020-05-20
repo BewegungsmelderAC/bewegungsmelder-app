c9e3f60847466c049a7d56d246eeedf5a81f623c
DocPaintfull <Frederic.kerksieck@rwth-aachen.de>
2020-05-20 18:19:40 +0200

    added component template

---
+++ app/components/Template.vue
@@ -0,0 +1,21 @@
+<template>
+  <Label :text="msg" ></Label>
+</template>
+
+<script >
+  // import { mapGetters } from 'vuex' // Necessary if the component requests data from store
+
+  export default {
+    data() {
+      return {
+        msg: 'Template' // Component data which are not part of the vuex store, ie. local data, go here
+      }
+    },
+    components: {
+    },
+    computed: {
+    },
+    methods: {
+    }
+  }
+</script>
